6574a8f88c3a3852d10a9300e95cd2fe79621037
Hunter Carrico <hmc.carrico@gmail.com>
2019-06-26 16:27:33 -0700

    minor picture change

--- src/Components/ProfilePhoto/ProfilePhoto.js
+++ src/Components/ProfilePhoto/ProfilePhoto.js
@@ -5,7 +5,7 @@ function ProfilePhoto(){
     return (
         <div className="profilePhoto">
             <div className="profilePhoto__photo">
-                <img src="https://lh3.googleusercontent.com/M9GOTCgM2LB7JBjj5z1Fmk3W-WzMtQY-k2xNkDxbV3rdOYhXQhOM4DfClO16jnLW1WeLCsAyiPtKCzDgqX_X6WLE-6RCAojxmUcqJgIzpVKX299oX7BaWzESAvQtD-vM_OiUWdE4vfuoZ7EGxci8i7FGY_hJMQlB5N5WxDI2fyn2p7qHaHgD7ExpyAOd5SSRUehepx0EGmHjNKcGFN9Tb-iIm6gbdFa7sG1Tq1N5I9SkhOc5XbuoRN5pJZrKvcx8qEGUCdTAsDeTPTHQfipv_jr_COErUwOkrokZX1p3tOaip3Z4Kk3w6Quh2zwOkat-DIpSWPrvKmCjsDc8fczibFw-n9lP_PafsssiG-ESbyxMLXXcFJDmlp4HiAH-tH25TYOc8JOf-zQiLtWZiUanvpQ_U8Wo-LIp4YoqkhEsMrks-27A1JDGJ3c0W8YdFCjuiqY63rgH8B9liFDibi88gyPaMXJCXOxtB6hfsg6VfeA5cA7EXiYZZucce62qporvlCdY8ODaxa-yuTh8PPbmogQsDMqd975BzX6qbD6QkObbEZTj-zOabbX8oM2FGeuvj-HZtlC7qIYuVTXMXdO8TPkpbL0ShUDRlai6FukSgnXu68d9rWoQYxwiRr_aO7icq-mFRvHgC7QSRKta9rHmI2TuWvEqUh0=s641-no" />
+                <img src="https://media.licdn.com/dms/image/C5603AQHJSPiHw8SZKg/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=409hEoFWsSw8JWrj75ehEIUOf25DuvfwxIywp7PuDBA" />
             </div>
             <h1>Hunter Carrico</h1>
         </div>
