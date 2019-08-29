ec60ce16ab9753d3c75d77bec6b23a03b84cfcf1
Hunter Carrico <hmc.carrico@gmail.com>
2019-08-29 11:42:01 -0700

    updated profile picture

--- src/Components/ProfilePhoto/ProfilePhoto.js
+++ src/Components/ProfilePhoto/ProfilePhoto.js
@@ -5,7 +5,7 @@ function ProfilePhoto(){
     return (
         <div className="profilePhoto">
             <div className="profilePhoto__photo">
-                <img src="https://media.licdn.com/dms/image/C5603AQHJSPiHw8SZKg/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=409hEoFWsSw8JWrj75ehEIUOf25DuvfwxIywp7PuDBA" />
+                <img src="https://media.licdn.com/dms/image/C5603AQHJSPiHw8SZKg/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=Rq9z9YWsSRU1ia2AQ2z-zvvlUzK4ZSD5HRrBNpBCxbc" />
             </div>
             <h2>Full Stack Web Developer</h2>
             <h1>Hunter Carrico</h1>
